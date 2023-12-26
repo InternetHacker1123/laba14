b9a07aedb9d704ebdd45311d86686f4515e23510
internetHacker1123 <dinozavrik9911@mail.ru>
2023-12-26 23:24:25 +0300

    add examples and task

---
+++ PyCharm/individual.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def outer_function(f):
+    def inner_function(a, b):
+        result = f(a, b)
+        return f"Для значений {a}, {b} функция f({a}, {b}) = {result}"
+    return inner_function
+
+
+# Пример функции f, которую мы передадим во внешнюю функцию
+def my_function(a, b):
+    return a * b + a - b
+
+
+# Создаем замыкание
+closure = outer_function(my_function)
+
+
+if __name__ == "__main__":
+    a = int(input("Введите первое число: "))
+    b = int(input("Введите второе число: "))
+    print(closure(a, b))
